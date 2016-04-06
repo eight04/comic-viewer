4116c73356d769f28f2848647009c8dfef135c34
eight <eight04@gmail.com>
2016-04-06 12:00:03 +0800

    Use docopt

--- comic-viewer.js
+++ comic-viewer.js
@@ -1,7 +1,22 @@
-var webViewer = require("web-viewer"),
+var doc = `Comic Viewer
+
+Usage
+  comic-viewer [--port <port>] [--no-server] <file>
+  comic-viewer --help | --version
+  
+Options:
+  --port       Set the port of the server. [default: 8080]
+  --no-server  Launch the file without starting comic-viewer server.
+  --help       Show help message.
+  --version    Show version number.`;
+
+var docopt = require("docopt"),
+	arguments = docopt(doc, {version: "0.1.0"});
+	webViewer = require("web-viewer"),
 	opener = require("opener"),
-	app = webViewer();
-	
+	app = webViewer(),
+	PORT = 8080;
+
 app.file(function(req, res){
 	// render reader
 	// req.path ...
@@ -11,6 +26,4 @@ app.dir(function(req, res){
 	// render file list
 });
 
-app.listen(8080, function(){
-	opener("http://localhost:" + PORT);
-});
+app.listen(PORT);
