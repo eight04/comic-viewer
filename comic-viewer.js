b10a99bf197af6e161e02e850c517e563e49e8e3
eight <eight04@gmail.com>
2016-04-06 20:04:37 +0800

    Fix docopt bug

--- comic-viewer.js
+++ comic-viewer.js
@@ -1,39 +1,57 @@
 var doc = `Comic Viewer
 
-Usage
-  comic-viewer [--port <port>] [--no-server] [--path <file>]
+Usage:
+  comic-viewer [--port <port>] [--no-server] [<file>]
   comic-viewer --help | --version
   
 Options:
-  --port       Set the port of the server. [default: 8080]
-  --no-server  Launch the file without starting comic-viewer server.
-  --help       Show help message.
-  --version    Show version number.`;
+  --port <port>  Set the port of the server. [default: 8080]
+  --no-server    Launch the file without starting comic-viewer server.
+  <file>         The file path. Open URL if provided. (http://localhost:8080/
+                 view?path=<file>)
+  --help         Show help message.
+  --version      Show version number.`;
 
 var docopt = require("docopt"),
-	args = docopt(doc, {version: "0.0.0"}),;
+	args = docopt.docopt(doc, {version: "0.0.0"});
 	
 function startServer() {	
 	var express = require("express"),
-		createWV = require("web-viewer"),
-		app = createWV(express());
+		createWV = require("./web-viewer"),
+		app = express();
 		
 	app.use("/jquery", express.static("bower_components/jquery/dist"));
 	
+	createWV(app);
+	
 	app.get("/view", function(req, res){
-		res.locals.dumps = JSON.stringify(res.locals);
+		res.locals.comicViewer = JSON.stringify({
+			isFile: res.locals.isFile,
+			curr: res.locals.curr,
+			next: res.locals.next,
+			prev: res.locals.prev
+		});
 		res.render("viewer.ejs");
 	});
 	
-	app.listen(args["--port"], launchFile);
+	app.listen(args["--port"], launchFile).on("error", handleError);
+}
+
+function handleError(err) {
+	if (err.code == "EADDRINUSE") {
+		launchFile();
+	} else {
+		throw err;
+	}
 }
 
 function launchFile() {
-	if (!args["--path"]) {
+	// console.log(args);
+	if (!args["<file>"]) {
 		return;
 	}
 	var opener = require("opener");
-	opener("http://localhost:" + args["--port"] + "/view?path=" + encodeURIComponent(args["--path"]));
+	opener("http://localhost:" + args["--port"] + "/view?path=" + encodeURIComponent(args["<file>"]));
 }
 
 if (!args["--no-server"]) {
