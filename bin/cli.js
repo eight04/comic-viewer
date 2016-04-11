a7850c76cb21cf2a524a415e03e49b438b441bea
eight <eight04@gmail.com>
2016-04-11 09:57:06 +0800

    Release v0.2.0

--- bin/cli.js
+++ bin/cli.js
@@ -20,7 +20,7 @@ var docopt = require("docopt"),
 	process = require("process"),
 	fs = require("fs"),
 	
-	args = docopt.docopt(doc, {version: "0.1.1"}),
+	args = docopt.docopt(doc, {version: "0.2.0"}),
 	path = require("path-extra"),
 	appDir = path.datadir("comic-viewer"),
 	lock = path.join(appDir, "lock"),
